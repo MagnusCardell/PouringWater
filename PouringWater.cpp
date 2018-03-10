093bc67258f4525e5735ce37491387389ea77fb0
MagnusCardell <cardell.magnus@gmail.com>
2018-03-10 15:37:40 -0600

    compiling version, boundary error.

--- PouringWater.cpp
+++ PouringWater.cpp
@@ -5,109 +5,119 @@
 #include <queue>
 
 using namespace std;
-struct Cups{
-    vector<vector<int>> states; 
-};
 
 class Node{
     int n;
-    Cups currState;
+    int **currState;
 
 public:
-    Node(){
-        n = 0;
-        currState.states = 0/10;
-        currState.B;
-        //waiting for logic...
+    Node(int ten, int seven, int four){
+        currState = new int*[3];
+        cout<<"SO FAR SO GOOD1"<<endl;
+        for(int i=0; i<2; ++i){
+            currState[i] = new int[2];
+        }
+        cout<<"SO FAR SO GOOD2"<<endl;
+        currState[0][0]=ten;
+        currState[0][1]=10;
+        currState[1][0]=seven;
+        currState[1][1]=7;
+        currState[2][0]=four;
+        currState[2][1]=4;
+        cout<<"SO FAR SO GOOD3"<<endl;
+    }
+    Node(int **curr){
+        currState = new int*[3];
+        for(int i=0; i<2; ++i){
+            currState[i] = new int[2];
+        }
+        for(int i=0; i<3; ++i){
+            for(int j=0; j<2; ++j){
+                currState[i][j]=curr[i][j];
+            }
+        }
     }
 
-    void genState(Cups n){
+    vector<int**> genState(){
         //Generate all valid neightbours
-        bool [] next = {true, true, true, true, true, true};
-        int [][] pos = {{0,1},{0,2},{1,2},{1,0},{2,0},{2,1}};
+        vector<int**> neighbours;
+        bool next[] = {true, true, true, true, true, true};
+        int pos[6][2] = {{0,1},{0,2},{1,2},{1,0},{2,0},{2,1}};
         int j = 0;
+        int **temp = new int*[3];
+        for(int i=0; i<2; ++i){
+            temp[i] = new int[2];
+        }
         while(j<3){
             /*if cup is full, all situations is is poured into
                 are false*/
             if(currState[j][0]==currState[j][1]){
                 for(int w = 0; w < 6; w++){
-                    if(poss[w][1]==(j+1))
-                        pos[w]=false;
+                    if(pos[w][1]==(j))
+                        next[w]=false;
                 }
             }
             /*if cup is empty, all situations is is poured from
                 are false*/
             if(currState[j][0]==0){
                 for(int w = 0; w < 6; w++){
-                    if(poss[w][0]==(j+1))
-                        next[w]==false;
+                    if(pos[w][0]==(j))
+                        next[w]=false;
                 }
             }
+            ++j;
         }
         int i = 0;
         while(i < 6){
+            copy( &currState[0][0], &currState[0][0]+3*2, &temp[0][0]);
             if(next[i]){
                 int maxFill = currState[pos[i][1]][1] - currState[pos[i][1]][0];
                 int inAmount = currState[pos[i][0]][0];
                 if(maxFill <= inAmount){
+                    
                     //create a child node with: 
-                    //currState[pos[i][0]][0]-=maxFill;
-                    //currState[pos[i][1]][0]+=maxFill;
+                    temp[pos[i][0]][0]-=maxFill;
+                    temp[pos[i][1]][0]+=maxFill;
+                    neighbours.push_back(temp);
                 }
                 else{
                     //new with with:
-                    //currState[pos[i][0]][0]=0;
-                    //currState[pos[i][1]][0]+=inAmount;
+                    temp[pos[i][0]][0]=0;
+                    temp[pos[i][1]][0]+=inAmount;
+                    neighbours.push_back(temp);
                 }
                 //create a child where that much has been poured. Do that for all possible situations.
             }
         }
-        children.push_back(n);
-        return;
+        return neighbours;
     }
 
-    //void addChild(char n){ }
-
-    vector<Cups> getChildren(){
-        return children;
+    int getState(int i){
+        return currState[i][0];
     }
 
-    bool operator==(Cups b){
-        return b.currState.A==currState.A; // add in checks for B and C
-    }
+    //bool operator==(Cups b){
+    //    return b.currState.A==currState.A; // add in checks for B and C
+    //}
 };
 
-
-//Stack implemented breadth first search
-/* Create   - a std::stack of Nodes for LIFO (last in first out)
-            - a vector children
-            - a string path
-    Add root to stack then start search until stack is empty
-            - Get the top Node
-            - add its value to path
-            - pop it out from the stack
-            - if Node is "goal" return path
-            - else loop over all children and add them to queue in reverse order
-            - while loop iterates
-    Upon completion of search return path (no success)
-*/
-string breadthFirstSearch(Cups root, Cups goal){
-    queue<Cups> FIFO_stack; //for saving stack order
-    map<Cups, int> control; // for checking visited Cups
-    vector<Cups> children; //for saving neighbours
+string breadthFirstSearch(Node root){
+    queue<Node> FIFO_stack; //for saving stack order
+    //map<Cups, int> control; // for checking visited Cups
+    vector<int**> children; //for saving neighbours
     string path = ""; //saving path? 
 
     FIFO_stack.push(root);
 
     while(!FIFO_stack.empty()){
-        Cups temp = FIFO_stack.front(); //Queue specific op
-        path += temp.n(); 
+        Node temp = FIFO_stack.front(); //Queue specific op
+        //path += temp.n(); 
         FIFO_stack.pop();
 
-        if(temp == goal){
-            return path;
+        if(temp.getState(1) == 2 or temp.getState(2) == 2){
+            return "found it";
         }
-        children = temp.genNeighbours();
+        children = temp.genState();
 
         for(int i=0; i< children.size(); ++i){
             FIFO_stack.push(children[i]);
@@ -116,6 +126,9 @@ string breadthFirstSearch(Cups root, Cups goal){
     return path;
 }
 int main(){
+    Node root(0,7,4);
+   
+    breadthFirstSearch(root);
     
 
     return 0;
